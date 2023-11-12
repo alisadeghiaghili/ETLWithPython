ac309d14a024e15a0d4ed6fa2961a2e9408bf9d2
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2023-11-12 14:56:32 +0330

    Update etl_code.py Add Transfrom function

--- etl_code.py
+++ etl_code.py
@@ -41,3 +41,14 @@ def extract():
         extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True) 
          
     return extracted_data 
+
+def transform(data): 
+    '''Convert inches to meters and round off to two decimals 
+    1 inch is 0.0254 meters '''
+    data['height'] = round(data.height * 0.0254,2) 
+ 
+    '''Convert pounds to kilograms and round off to two decimals 
+    1 pound is 0.45359237 kilograms '''
+    data['weight'] = round(data.weight * 0.45359237,2) 
+    
+    return data 
