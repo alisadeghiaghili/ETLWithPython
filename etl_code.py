b84575ee43de0ab989eac52b768c2959f74bae7b
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2023-11-12 14:55:33 +0330

    Create etl_code.py

---
+++ etl_code.py
@@ -0,0 +1,43 @@
+import glob 
+import pandas as pd 
+import xml.etree.ElementTree as ET 
+from datetime import datetime 
+
+log_file = "log_file.txt" 
+target_file = "transformed_data.csv" 
+
+def extract_from_csv(file_to_process): 
+    dataframe = pd.read_csv(file_to_process) 
+    return dataframe 
+
+def extract_from_json(file_to_process): 
+    dataframe = pd.read_json(file_to_process, lines=True) 
+    return dataframe 
+
+def extract_from_xml(file_to_process): 
+    dataframe = pd.DataFrame(columns=["name", "height", "weight"]) 
+    tree = ET.parse(file_to_process) 
+    root = tree.getroot() 
+    for person in root: 
+        name = person.find("name").text 
+        height = float(person.find("height").text) 
+        weight = float(person.find("weight").text) 
+        dataframe = pd.concat([dataframe, pd.DataFrame([{"name":name, "height":height, "weight":weight}])], ignore_index=True) 
+    return dataframe 
+
+def extract(): 
+    extracted_data = pd.DataFrame(columns=['name','height','weight']) # create an empty data frame to hold extracted data 
+     
+    # process all csv files 
+    for csvfile in glob.glob("*.csv"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True) 
+         
+    # process all json files 
+    for jsonfile in glob.glob("*.json"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))], ignore_index=True) 
+     
+    # process all xml files 
+    for xmlfile in glob.glob("*.xml"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True) 
+         
+    return extracted_data 
