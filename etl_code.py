a7db756341970079b81639c227072fbbe1e6abde
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2023-11-12 14:59:48 +0330

    Update etl_code.py complete the main body

--- etl_code.py
+++ etl_code.py
@@ -62,3 +62,33 @@ def log_progress(message):
     timestamp = now.strftime(timestamp_format) 
     with open(log_file,"a") as f: 
         f.write(timestamp + ',' + message + '\n') 
+
+
+# Log the initialization of the ETL process 
+log_progress("ETL Job Started") 
+ 
+# Log the beginning of the Extraction process 
+log_progress("Extract phase Started") 
+extracted_data = extract() 
+ 
+# Log the completion of the Extraction process 
+log_progress("Extract phase Ended") 
+ 
+# Log the beginning of the Transformation process 
+log_progress("Transform phase Started") 
+transformed_data = transform(extracted_data) 
+print("Transformed Data") 
+print(transformed_data) 
+ 
+# Log the completion of the Transformation process 
+log_progress("Transform phase Ended") 
+ 
+# Log the beginning of the Loading process 
+log_progress("Load phase Started") 
+load_data(target_file,transformed_data) 
+ 
+# Log the completion of the Loading process 
+log_progress("Load phase Ended") 
+ 
+# Log the completion of the ETL process 
+log_progress("ETL Job Ended") 
