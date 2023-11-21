c1b319321108169d58c911c565f17d8a471474f9
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2023-11-21 14:01:52 +0330

    Update banks_project.py

--- banks_project.py
+++ banks_project.py
@@ -6,8 +6,8 @@ import sqlite3
 from datetime import datetime 
 
 url = 'https://web.archive.org/web/20230908091635%20/https://en.wikipedia.org/wiki/List_of_largest_banks'
+rates_csv_path = './exchange_rate.csv'
 scraped_table_attribs = ["Name", "MC_USD_Billion"]
-final_table_attribs = ["Name", "MC_USD_Billion", "MC_GBP_Billion", "MC_EUR_Billion", "MC_INR_Billion"]
 db_name = 'Banks.db'
 table_name = 'Largest_banks'
 csv_path = './Largest_banks_data.csv'
@@ -36,7 +36,7 @@ def extract(url, table_attribs):
     for row in rows:
         col = row.find_all('td')
         data_dict = {"Name": col[1].text.replace('\n', ""),
-                     "MC_USD_Billion": col[2].text.replace('\n', "")}
+                     "MC_USD_Billion": float(col[2].text.replace('\n', ""))}
         df1 = pd.DataFrame(data_dict, index=[0])
         df = pd.concat([df,df1], ignore_index=True)
 
@@ -48,25 +48,37 @@ def transform(df, csv_path):
     containing the transformed version of Market Cap column to
     respective currencies'''
     
+    exchange_rate = pd.read_csv(csv_path).set_index('Currency').to_dict()['Rate']
+    df['MC_GBP_Billion'] = [np.round(x * exchange_rate['GBP'],2) for x in df['MC_USD_Billion']]
+    df['MC_EUR_Billion'] = [np.round(x * exchange_rate['EUR'],2) for x in df['MC_USD_Billion']]
+    df['MC_INR_Billion'] = [np.round(x * exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
     
-
     return df
 
 def load_to_csv(df, output_path):
     ''' This function saves the final data frame as a CSV file in
     the provided path. Function returns nothing.'''
+    
+    df.to_csv(csv_path)
 
 def load_to_db(df, sql_connection, table_name):
     ''' This function saves the final data frame to a database
     table with the provided name. Function returns nothing.'''
+    
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
 
 def run_query(query_statement, sql_connection):
     ''' This function runs the query on the database table and
     prints the output on the terminal. Function returns nothing. '''
+    
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+    
 
-''' Here, you define the required entities and call the relevant
-functions in the correct order to complete the project. Note that this
-portion is not inside any function.'''
+# ''' Here, you define the required entities and call the relevant
+# functions in the correct order to complete the project. Note that this
+# portion is not inside any function.'''
 
 
 log_progress('Preliminaries complete. Initiating ETL process')
@@ -74,9 +86,29 @@ log_progress('Preliminaries complete. Initiating ETL process')
 df = extract(url, scraped_table_attribs)
 
 log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df, rates_csv_path)
+
 log_progress('Data transformation complete. Initiating Loading process')
+
+load_to_csv(df, csv_path)
+
 log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect(db_name)
+
 log_progress('SQL Connection initiated')
+
+load_to_db(df, sql_connection, table_name)
+
 log_progress('Data loaded to Database as a table, Executing queries')
+
+run_query('SELECT * FROM Largest_banks', sql_connection)
+run_query('SELECT AVG(MC_GBP_Billion) FROM Largest_banks', sql_connection)
+run_query('SELECT Name from Largest_banks LIMIT 5', sql_connection)
+
 log_progress('Process Complete')
+
+sql_connection.close()
+
 log_progress('Server Connection closed')
